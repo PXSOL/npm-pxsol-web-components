9935fa7d234bb67edaffcbfdf8c3475059911cfd
Santiago Cabral <scabral@pxsol.com>
2025-03-09 19:14:47 -0300

    Fix: Resolver problemas con rutas de módulos

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { default as Hero } from "./components/Hero";
 export { default as About } from "./components/About";
 export { default as Cta } from "./components/Cta";
 export { default as WidgetChat } from "./components/WidgetChat";
+
+
