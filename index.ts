929f14a7b705ab07dded77f8626b6c2d11cbbbb0
Santiago Cabral <scabral@pxsol.com>
2025-03-09 18:08:20 -0300

    Agregando index.css y exportándolo

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from "./src/components/Header";
+export { default as Hero } from "./src/components/Hero";
+export { default as About } from "./src/components/About";
+export { default as Cta } from "./src/components/Cta";
+export { default as WidgetChat } from "./src/components/WidgetChat";
